0c8d5bf42c646d8c94593b0e26b570a522078390
Your Name <mwaurarita2019@gmail.com>
2019-10-14 10:29:31 +0300

    commitAdd getting returning render template

--- app/main/views.py
+++ app/main/views.py
@@ -6,3 +6,21 @@ from ..models import news_Article
 @main.route('/')
 def index():
     
+    '''
+    View root page function that returns the index page and its data
+    '''
+    #getting news sources based on the category from the news sources endpoint
+    general = get_news_sources('general')
+    business = get_news_sources('business')
+    technology = get_news_sources('technology')
+    health = get_news_sources('health')
+    science = get_news_sources('science')
+    sports = get_news_sources('sports')
+    
+    title = 'The best news Highlits In The World'
+    
+    search_news_source = request.args.get('news_query')
+   if search_news_source:
+       return redirect(url_for('main.index',news_name = search_news_source))
+    else:
+        return render_template('index.html',title = title,general = general ,business = business, technology = technology,health=health,science=science,sports=sports)

a95bbc39b501599bf7bd4bb2b76998745d9ec1a4
Your Name <mwaurarita2019@gmail.com>
2019-10-14 12:45:07 +0300

    Add define the news sources

--- app/models.py
+++ app/models.py
@@ -2,3 +2,13 @@ class news_Source:
     '''
     news_Source class to define news_Source Objects
     '''
+    def __init__(self,newsid,author,title,description,url,urlToImage,publishedAt,content):
+        self.newsid = newsid
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+

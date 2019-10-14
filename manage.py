f5c647b6110c8baff958cc1b838b32c0fe24fff4
Your Name <mwaurarita2019@gmail.com>
2019-10-14 14:30:33 +0300

    Add class and define test

--- manage.py
+++ manage.py
@@ -5,3 +5,9 @@ from flask_script import Manager,Shell,Server
 app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)

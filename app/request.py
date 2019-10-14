e67230b85270bbe9fe6d3a39ddb1458d0f2b5787
Your Name <mwaurarita2019@gmail.com>
2019-10-14 12:52:02 +0300

    Add articles configuration

--- app/request.py
+++ app/request.py
@@ -7,3 +7,10 @@ api_key = None
 # Getting the movie base url
 base_url = None
 articles_url=None
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+    # articles_url=app.config['NEWS_API_ARTICLES_URL']
+
+

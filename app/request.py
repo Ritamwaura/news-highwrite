773964a2c601523592a612c784d49a66c4a89183
Your Name <mwaurarita2019@gmail.com>
2019-10-14 12:55:21 +0300

    Add return news source result

--- app/request.py
+++ app/request.py
@@ -28,4 +28,48 @@ def get_news_sources(category):
         if get_news_source_response['sources']:
             news_source_results_list = get_news_source_response['sources']
             news_source_results = process_results(news_source_results_list)
+ return news_source_results
+def get_news_source(source):
+    '''
+    Function that gets the json responce to our url request
+    '''
+    get_news_source_url = base_url.format(source,api_key)
 
+    with urllib.request.urlopen(get_news_source_url) as url:
+        get_news_source_data = url.read()
+        get_news_source_response = json.loads(get_news_source_data)
+
+        news_source_results = None
+
+        if get_news_source_response['sources']:
+            news_source_results_list = get_news_source_response['sources']
+            news_source_results = process_results(news_source_results_list)
+
+
+    return news_source_results
+
+
+def process_results(news_source_results):
+    '''
+    Function  that processes the news source result and transform them to a list of Objects
+    Args:
+        news_sources_list: A list of dictionaries that contain news details
+    Returns :
+        news_source_results: A list of news sources objects
+    '''
+    news_sources_results = []
+    
+    for news_source_item in news_sources_results:
+        id = news_source_item.get('id')
+        author = news_source_item.get('author')
+        title = news_source_item.get('title')
+        description = news_source_item.get('description')
+        url = news_source_item.get('url')
+        urlToImage = news_source_item.get('urlToImage')
+        publishedAt = news_source_item.get('publishedAt')
+        content = news_source_item.get('content')
+
+        news_source_object = news_Source(id,author,title,description,url,urlToImage,publishedAt,content)
+        news_sources_results.append(news_source_object)
+
+    return news_source_results

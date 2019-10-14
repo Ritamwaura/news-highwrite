b636d640221d24435667a9c4f875b9848afef4b5
Your Name <mwaurarita2019@gmail.com>
2019-10-14 14:57:01 +0300

    Add identation

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -5,14 +5,14 @@ from . import views,error
 def create_app(config_name):
     app = Flask(__name__)
 
-# Creating the app configurations
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-# Initializing flask extensions
+    # Initializing flask extensions
     bootstrap.init_app(app)
 
     # Registering the blueprint
-     from .main import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app

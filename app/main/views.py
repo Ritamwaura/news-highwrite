36408cd87888e6ac30a6d41cc8a4535d355c9550
Your Name <mwaurarita2019@gmail.com>
2019-10-14 10:14:55 +0300

    commitAdd models class to to news article

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,3 @@
 from flask import render_template,request,redirect,url_for 
- from ..request import get_news_sources,get_news_source
+from ..request import get_news_sources,get_news_source
+from ..models import news_Article

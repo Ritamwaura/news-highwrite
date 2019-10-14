2e7e4005bfd456efba9d14003e0d915d4589d462
Your Name <mwaurarita2019@gmail.com>
2019-10-14 12:54:00 +0300

    Add a funtion that gets the json responce

--- app/request.py
+++ app/request.py
@@ -13,4 +13,19 @@ def configure_request(app):
     base_url = app.config['NEWS_API_BASE_URL']
     # articles_url=app.config['NEWS_API_ARTICLES_URL']
 
+def get_news_sources(category):
+    '''
+    Function that gets the json responce to our url request
+    '''
+    get_news_source_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_source_url) as url:
+        get_news_source_data = url.read()
+        get_news_source_response = json.loads(get_news_source_data)
+
+        news_source_results = None
+
+        if get_news_source_response['sources']:
+            news_source_results_list = get_news_source_response['sources']
+            news_source_results = process_results(news_source_results_list)
 

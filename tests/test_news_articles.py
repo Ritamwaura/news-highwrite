7d3d3b3f94cd6c244fe9c64ad328cc358caa4865
Your Name <mwaurarita2019@gmail.com>
2019-10-14 13:03:24 +0300

    Add creating a class  for news article

--- tests/test_news_articles.py
+++ tests/test_news_articles.py
@@ -1,2 +1,8 @@
 import unittest
 from app.models import news_Article
+class Test__news_Article(unittest.TestCase):
+    
+    def setUp(self):
+        self.new_article = news_Article(1,'The numbers are going up but not very much','Fox News Today','https://www.cnbc.com/2019/07/26/us-gdp-second-quarter-2019.html','Growth decelerated in the second quarter but not by as much as Wall Street thought')
+
+
